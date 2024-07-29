c93b3151057d885a28b5513fbf5905552e80123b
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-07-29 09:41:19 +0300

    feat: worked on the desktop version of TotalBackers

--- src/components/TotalBackers.jsx
+++ src/components/TotalBackers.jsx
@@ -4,21 +4,23 @@ import ProgressBar from './ui/ProgressBar';
 const TotalBackers = () => {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.item}>
-        <p className={classes.title}>$89,914</p>
-        <p className={classes['under-title']}>of $100,000 backed</p>
-        <div className={classes.line}></div>
+      <div className={classes['inner-wrapper']}>
+        <div className={classes.item}>
+          <p className={classes.title}>$89,914</p>
+          <p className={classes['under-title']}>of $100,000 backed</p>
+          <div className={classes.line}></div>
+        </div>
+        <div className={classes.item}>
+          <p className={classes.title}>5,007</p>
+          <p className={classes['under-title']}>total backers</p>
+          <div className={classes.line}></div>
+        </div>
+        <div className={classes.item}>
+          <p className={classes.title}>56</p>
+          <p className={classes['under-title']}>days left</p>
+        </div>
       </div>
-      <div className={classes.item}>
-        <p className={classes.title}>5,007</p>
-        <p className={classes['under-title']}>total backers</p>
-        <div className={classes.line}></div>
-      </div>
-      <div className={classes.item}>
-        <p className={classes.title}>56</p>
-        <p className={classes['under-title']}>days left</p>
-        <ProgressBar />
-      </div>
+      <ProgressBar />
     </div>
   );
 };
