68ec94a71217e36b38d0585d777d19dd30d40603
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-07-29 10:21:45 +0300

    feat: worked on desktop version of ModalPledge

--- src/components/ui/ModalPledge.jsx
+++ src/components/ui/ModalPledge.jsx
@@ -33,46 +33,93 @@ const ModalPledge = ({
 
   return (
     <div id={`modal-pledge-${id}`} className={wrapperClasses}>
-      <div className={classes['pledge-inner']}>
-        <div className={classes['checkbox-wrapper']}>
-          <Checkbox
-            onClick={disableCheckbox ? () => {} : () => selectPledge(id)}
-            isActive={isSelected}
-            disabled={disableCheckbox}
-          />
-          <div className={classes['title-wrapper']}>
-            <p className={classes.title}>{title}</p>
-            {showMinimumPledge && (
-              <p className={classes['minimum-pledge']}>
-                Pledge ${minimumPledge} or more
-              </p>
-            )}
+      <div className={classes['mobile-pledge']}>
+        <div className={classes['pledge-inner']}>
+          <div className={classes['checkbox-wrapper']}>
+            <Checkbox
+              onClick={disableCheckbox ? () => {} : () => selectPledge(id)}
+              isActive={isSelected}
+              disabled={disableCheckbox}
+            />
+            <div className={classes['title-wrapper']}>
+              <p className={classes.title}>{title}</p>
+              {showMinimumPledge && (
+                <p className={classes['minimum-pledge']}>
+                  Pledge ${minimumPledge} or more
+                </p>
+              )}
+            </div>
           </div>
+          <p className={classes.description}>{description}</p>
+          {showMinimumPledge && (
+            <div className={classes['pledges-left-wrapper']}>
+              <p className={classes['pledges-left']}>{remainingPledges}</p>
+              <p className={classes['pledges-left-text']}>left</p>
+            </div>
+          )}
         </div>
-        <p className={classes.description}>{description}</p>
-        {showMinimumPledge && (
-          <div className={classes['pledges-left-wrapper']}>
-            <p className={classes['pledges-left']}>{remainingPledges}</p>
-            <p className={classes['pledges-left-text']}>left</p>
+        {isSelected && showMinimumPledge && (
+          <div className={classes['enter-pledge-wrapper']}>
+            <div className={classes['pledge-inner']}>
+              <p className={classes['enter-pledge-title']}>Enter your pledge</p>
+              <div className={classes['enter-pledge-inner']}>
+                <AmountInput
+                  prefix='$'
+                  value={pledge}
+                  minValue={minimumPledge}
+                  maxValue={maximumPledge}
+                />
+                <Button onClick={showThankYouModal} title='Continue' />
+              </div>
+            </div>
           </div>
         )}
       </div>
-      {isSelected && showMinimumPledge && (
-        <div className={classes['enter-pledge-wrapper']}>
-          <div className={classes['pledge-inner']}>
-            <p className={classes['enter-pledge-title']}>Enter your pledge</p>
-            <div className={classes['enter-pledge-inner']}>
-              <AmountInput
-                prefix='$'
-                value={pledge}
-                minValue={minimumPledge}
-                maxValue={maximumPledge}
-              />
-              <Button onClick={showThankYouModal} title='Continue' />
+      <div className={classes['desktop-pledge']}>
+        <div className={classes['pledge-inner']}>
+          <div className={classes['checkbox-wrapper']}>
+            <Checkbox
+              onClick={disableCheckbox ? () => {} : () => selectPledge(id)}
+              isActive={isSelected}
+              disabled={disableCheckbox}
+            />
+            <div className={classes['title-pledges-left-wrapper']}>
+              <div className={classes['title-minimum-pledge-wrapper']}>
+                <p className={classes.title}>{title}</p>
+                {showMinimumPledge && (
+                  <p className={classes['minimum-pledge']}>
+                    Pledge ${minimumPledge} or more
+                  </p>
+                )}
+              </div>
+
+              {showMinimumPledge && (
+                <div className={classes['pledges-left-wrapper']}>
+                  <p className={classes['pledges-left']}>{remainingPledges}</p>
+                  <p className={classes['pledges-left-text']}>left</p>
+                </div>
+              )}
             </div>
           </div>
+          <p className={classes.description}>{description}</p>
         </div>
-      )}
+        {isSelected && showMinimumPledge && (
+          <div className={classes['enter-pledge-wrapper']}>
+            <div className={classes['pledge-inner']}>
+              <p className={classes['enter-pledge-title']}>Enter your pledge</p>
+              <div className={classes['enter-pledge-inner']}>
+                <AmountInput
+                  prefix='$'
+                  value={pledge}
+                  minValue={minimumPledge}
+                  maxValue={maximumPledge}
+                />
+                <Button onClick={showThankYouModal} title='Continue' />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
